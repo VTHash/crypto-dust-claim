c1eb3f5bfc80efff708429623db4a5d2cbf5139b
VTHash <hfv@highfinanceventures.com>
2025-10-26 23:07:22 +0000

    Add new Abi + contract op mainnet

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -6,6 +6,7 @@ import bnbAbi from './contracts/dustclaim.bnb.json'
 import avaxAbi from './contracts/dustclaim.avax.json'
 import arbAbi from './contracts/dustclaim.arb.json'
 import polygonAbi from './contracts/dustclaim.polygon.json'
+import opAbi from './contracts/dustclaim.op.json'
 
 // ---- Back-compat: expose the default ABI the rest of the app expects ----
 export const DUSTCLAIM_ABI = commonAbi
@@ -13,7 +14,7 @@ export const DUSTCLAIM_ABI = commonAbi
 // ---- Addresses per chain (public – fine to keep in repo) ----
 export const DUSTCLAIM_ADDRESS = {
   1: "0x73f2Ef769b3Dc5c84390347b05cc1D89dD9644f", // Ethereum ✅
-  10: "", // OP Mainnet
+  10: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46", // OP Mainnet
   56: "0x8794D4CD9b641eD623235ca418640e10E4d75D6F", // BNB Smart Chain
   100: "", // Gnosis
   137: "0xf977f21430b99aE91680aC2e0fFD8cA481ec486F", // Polygon PoS
