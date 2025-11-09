52beca115775ac8b9ffe67defe0f98e646d6cd24
VTHash <hfv@highfinanceventures.com>
2025-11-09 17:55:11 +0000

    Add new contract abi and import

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -15,6 +15,8 @@ import zoraAbi from './contracts/dustclaim.zora.json'
 import beraAbi from './contracts/dustclaim.bera.json'
 import fantomAbi from './contracts/dustclaim.fantom.json'
 import unichainAbi from './contracts/dustclaim.unichain.json'
+import celoAbi from './contracts/dustclaim.celo.json'
+
 // ---- Back-compat: expose the default ABI the rest of the app expects ----
 export const DUSTCLAIM_ABI = commonAbi
 
@@ -35,7 +37,8 @@ export const DUSTCLAIM_ADDRESS = {
   59144: "0xEB4931BE941D830425420D1Ba7206e8E43854795", // Linea
   80094: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46", // Berachain (matches your SUPPORTED_CHAINS)
   7777777: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46", // Zora
-  130: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46" // Unichain
+  130: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46", // Unichain
+  42220: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46" // Celo
 }
 
 // ---- Optional: per-chain ABI overrides (only add if different from common) ----
