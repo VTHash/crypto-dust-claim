ff6f7d1f8ffd8c5a1a638cf85f4ae084e079152e
VTHash <hfv@highfinanceventures.com>
2025-11-13 21:38:34 +0000

    Add changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -118,6 +118,7 @@ export default function Navbar() {
           )}
         </div>
         
+        {/* Coingecko ticker row inside navbar */}
         <div className="coin-marquee">
         <gecko-coin-price-marquee-widget
           locale="en"
