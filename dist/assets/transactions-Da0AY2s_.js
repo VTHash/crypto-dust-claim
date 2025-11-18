fbe4cc2c172ec246d9e782ffbd20b8b98df0dec3
VTHash <hfv@highfinanceventures.com>
2025-11-18 22:40:54 +0000

    Add globalStats minipanel

---
+++ dist/assets/transactions-Da0AY2s_.js
@@ -0,0 +1,16 @@
+import{o as f,n as a,x as m}from"./index-jhDdO9e9.js";import{c as p}from"./index-BI7AZc9x.js";import"./index-Dk_RvFi6.js";import"./index-DmGam5ds.js";import"./if-defined-uSy4kBlM.js";import"./index-DKhavl4F.js";import"./index-CmQlHA6V.js";import"./index-BrkCnAPi.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
