38642907830145198d7e4a10462af6755079117a
VTHash <hfv@highfinanceventures.com>
2025-12-12 23:36:26 +0000

    Refactor CHAIN_CAPS

--- src/config/chainCapabilities.js
+++ src/config/chainCapabilities.js
@@ -1,28 +1,31 @@
-// If at least one aggregator route is reliably available, set directSwap:true.
-// uniV3Path is specifically for your Uniswap V3 single-path button.
+// src/config/chainCapabilities.js
+// 0x-only routing support flags
 
 export const CHAIN_CAPS = {
-  1: { directSwap: true, uniV3Path: true }, // Ethereum
-  10: { directSwap: true, uniV3Path: true }, // Optimism
-  56: { directSwap: true, uniV3Path: true }, // BNB (keep uniV3 off unless you wire it)
-  137: { directSwap: true, uniV3Path: true }, // Polygon
-  42161: { directSwap: true, uniV3Path: true }, // Arbitrum
-  8453: { directSwap: true, uniV3Path: true }, // Base
-  43114: { directSwap: true, uniV3Path: true }, // Avalanche
-
-  100: { directSwap: true, uniV3Path: true }, // Gnosis (conservative)
-  250: { directSwap: true, uniV3Path: false }, // Fantom
+  1: { directSwap: true, uniV3Path: false }, // Ethereum
+  10: { directSwap: true, uniV3Path: false }, // Optimism
+  56: { directSwap: true, uniV3Path: false }, // BSC
+  130: { directSwap: true, uniV3Path: false }, // Unichain
+  137: { directSwap: true, uniV3Path: false }, // Polygon
+  143: { directSwap: true, uniV3Path: false }, // Monad
+  146: { directSwap: true, uniV3Path: false }, // Sonic
+  480: { directSwap: true, uniV3Path: false }, // World Chain
+  5000: { directSwap: true, uniV3Path: false }, // Mantle
+  9745: { directSwap: true, uniV3Path: false }, // Plasma
+  42161: { directSwap: true, uniV3Path: false }, // Arbitrum
+  43114: { directSwap: true, uniV3Path: false }, // Avalanche
+  534352: { directSwap: true, uniV3Path: false }, // Scroll
   59144: { directSwap: true, uniV3Path: false }, // Linea
-  7777777: { directSwap: false, uniV3Path: true }, // Zora
-  34443: { directSwap: false, uniV3Path: false }, // Mode
-  1329: { directSwap: false, uniV3Path: false }, // Sei
-  80094: { directSwap: false, uniV3Path: false }, // Berachain
- 
-};
+  80094: { directSwap: true, uniV3Path: false }, // Berachain
+  81457: { directSwap: true, uniV3Path: false }, // Blast
+  34443: { directSwap: true, uniV3Path: false }, // Mode
+  8453: { directSwap: true, uniV3Path: false }, // Base
+  57073: { directSwap: true, uniV3Path: false }, // Ink
+}
 
 // Optional helpers (non-breaking)
 export const canDirectSwap = (chainId) =>
-  !!(CHAIN_CAPS[Number(chainId)] && CHAIN_CAPS[Number(chainId)].directSwap);
+  !!(CHAIN_CAPS[Number(chainId)] && CHAIN_CAPS[Number(chainId)].directSwap)
 
 export const hasUniV3Path = (chainId) =>
-  !!(CHAIN_CAPS[Number(chainId)] && CHAIN_CAPS[Number(chainId)].uniV3Path);
+  !!(CHAIN_CAPS[Number(chainId)] && CHAIN_CAPS[Number(chainId)].uniV3Path)
