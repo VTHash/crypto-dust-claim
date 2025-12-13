fa48777c0a1085b274eade2dc6135401d3b77bc1
VTHash <hfv@highfinanceventures.com>
2025-12-13 22:28:21 +0000

    Added all new deployments contracts, addresses, supported chains, correct logos, new route aggregator and services

--- src/config/deployments.js
+++ src/config/deployments.js
@@ -1,61 +1,60 @@
 // Per-chain execution config for swaps/claims.
 // - `weth`: the WRAPPED *native* token of that chain (ERC-20 form used for swaps).
-// - `oneInchRouter`: 1inch v5 router where supported; else null.
-// - `uniswapV3Router`: Uniswap v3/Universal Router (or compatible) where present; else null.
+
 
 export const DEPLOYMENTS = {
   1: {
     chainId: 1,
     name: 'Ethereum',
-    dustclaim: '0x73f2Ef769b3Dc5c84390347b05cc1D89dD9644f',
+    dustclaim: '0xa87B722979D3c2D381A225E224427498455d535e',
     weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://api.0x.org/',
+    support0x: true,
   },
 
   10: {
     chainId: 10,
     name: 'Optimism',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xEB4931BE941D830425420D1Ba7206e8E43854795',
     weth: '0x4200000000000000000000000000000000000006',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://optimism.api.0x.org/',
+    support0x: true,
   },
 
   56: {
     chainId: 56,
     name: 'BNB Smart Chain',
-    dustclaim: '0x8794D4CD9b641eD623235ca418640e10E4d75D6F',
+    dustclaim: '0xC9b01707cE50803783ECcD0A995233Ab3052Fd1A',
     weth: '0xBB4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://bsc.api.0x.org/',
+    support0x: true,
   },
 
   100: {
     chainId: 100,
     name: 'Gnosis',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: null,
+    support0x: false,
   },
 
   130: {
     chainId: 130,
     name: 'Unichain',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0x4200000000000000000000000000000000000006',
-    oneInchRouter: null,
-    uniswapV3Router: null
+    zeroXHost: 'https://unichain.api.0x.org/',
+    support0x: true,
   },
 
   137: {
     chainId: 137,
     name: 'Polygon PoS',
-    dustclaim: '0xf977f21430b99aE91680aC2e0fFD8cA481ec486F',
+    dustclaim: '0x6f04783806684760f841b981d1823b46584200D8',
     weth: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://polygon.api.0x.org/',
+    support0x: true,  
   },
 
 
@@ -64,165 +63,162 @@ export const DEPLOYMENTS = {
     name: 'Fantom Opera',
     dustclaim: '0xe6292481711419e6035b8Ac263Fd91AF48142966',
     weth: '0x21be370D5312f44cb42ce377BC9b8a0cef1A4C83',
-    oneInchRouter: null,
-    uniswapV3Router: null
+    zeroXHost: null,
+    support0x: false,
   },
 
   1284: {
     chainId: 1284,
     name: 'Moonbeam',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0xAcc15dC74880C9944775448304B263D191c6077F',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: null
+    zeroXHost: null,
+    support0x: false,
   },
 
   1285: {
     chainId: 1285,
     name: 'Moonriver',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0x98878B06940aE243284CA214f92Bb71a2b032B8A',
-    oneInchRouter: null,
-    uniswapV3Router: null
+   zeroXHost: null,
+    support0x: false,
   },
 
   1329: {
     chainId: 1329,
     name: 'Sei Network',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0x7692fDf5bbcA49ACE485D31B760e8A082d193D3d',
     weth: '0xE30FeDd158A2e3b1e39EbaeABaFc5516e95e98C7',
-    oneInchRouter: null,
-    uniswapV3Router: null
+    zeroXHost: null,
+    support0x: false,
   },
 
   42220: {
     chainId: 42220,
     name: 'Celo',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0x7692fDf5bbcA49ACE485D31B760e8A082d193D3d',
     weth: '0x471EcE3750Da237f93B8E339c536989b8978a438',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: null
+    zeroXHost: null,
+    support0x: false,
   },
 
   42161: {
     chainId: 42161,
     name: 'Arbitrum One',
-    dustclaim: '0xd7aC005D908Cbf7A9692478c4DEef2525CA2A2fE',
+    dustclaim: '0x003031Aef54ED627Cf7b9783802C86BBB05d1e72',
     weth: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://arbitrum.api.0x.org/',
+    support0x: true,
   },
 
   43114: {
     chainId: 43114,
     name: 'Avalanche C-Chain',
-    dustclaim: '0xA10980211Cda7228708e774ef11c7E299E6947dB',
+    dustclaim: '0xe41a31664DaCf9cE696545Cf770e7F6662CF61fd',
     weth: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://avalanche.api.0x.org/',
+    support0x: true,
   },
 
   5000: {
     chainId: 5000,
     name: 'Mantle',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: null
+    zeroXHost: 'https://mantle.api.0x.org/',
+    support0x: true,
   },
 
   59144: {
     chainId: 59144,
     name: 'Linea',
-    dustclaim: '0xEB4931BE941D830425420D1Ba7206e8E43854795',
+    dustclaim: '0xBB45cc85B5e6505Ad1C8403227Da68ba0F13357B',
     weth: '0xE5D7C2a44FfDDf6b295A15c148167daaAf5Cf34F',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://linea.api.0x.org/',
+    support0x: true,
   },
 
   8453: {
     chainId: 8453,
     name: 'Base',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xBfc253Ffc3fDD5533D91937f062bf0CD7d4A1551',
     weth: '0x4200000000000000000000000000000000000006',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+    zeroXHost: 'https://base.api.0x.org/',
+    support0x: true,
   },
 
   34443: {
     chainId: 34443,
     name: 'Mode',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0x4200000000000000000000000000000000000006',
-    oneInchRouter: null,
-    uniswapV3Router: null
+    zeroXHost: 'https://mode.api.0x.org/',
+    support0x: true,
   },
 
   7777777: {
     chainId: 7777777,
     name: 'Zora',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0x4200000000000000000000000000000000000006',
-    oneInchRouter: null,
-    uniswapV3Router: '0x7De04c96BE5159c3b5CeffC82aa176dc81281557'
+    zeroXHost: null,
+    support0x: false,
   },
 
   80094: {
     chainId: 80094,
     name: 'Berachain',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0x6969696969696969696969696969696969696969',
-    oneInchRouter: null,
-    uniswapV3Router: null
+    zeroXHost: 'https://berachain.api.0x.org/',
+    support0x: true,
   },
 
   9745: {
     chainId: 9745,
     name: 'Plasma',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0x4200000000000000000000000000000000000006',
-    oneInchRouter: null,
-    uniswapV3Router: null
+    zeroXHost: 'https://plasma.api.0x.org/',
+    support0x: true,
   },
 
   1313161554: {
     chainId: 1313161554,
     name: 'Aurora',
-    dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
+    dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
     weth: '0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB',
-    oneInchRouter: '0x1111111254EEB25477B68fb85Ed929f73A960582',
-    uniswapV3Router: null
+    zeroXHost: null,
+    support0x: false,
   },
 
   14: {
   chainId: 14,
   name: 'Flare',
-  dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46', // placeholder per your pattern
+  dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc', // placeholder per your pattern
   weth: '0x1D80c49bBbCd1c0911346656B529dF9E5c2F783d', // WFLR (native wrapper)
-  oneInchRouter: '0x0000000000000000000000000000000000000000', 
-  uniswapV3Router: '0x8a1E35F5c98C4E85B36B7B253222eE17773b2781' // Flare v3 router
+  zeroXHost: null, // 0x not yet supported on Flare
+  support0x: false,
 },
-
-
-
 
 40: {
   chainId: 40,
   name: 'Telos',
-  dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
-  weth: '0xB6C53431608E626AC81a9776ac3e999c5556717c', // WTLOS
-  oneInchRouter: null, // 1inch not on Telos
-  uniswapV3Router: null // no Uniswap v3
+  dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
+  weth: '0xBAb93B7ad7fE8692A878B95a8e689423437cc500', // WTLOS
+  zeroXHost: null,
+  support0x: false,
 },
 
 
 57: {
   chainId: 57,
   name: 'Syscoin',
-  dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
-  weth: '0x27fE1F0aBdDFD2AAdb2411AF6B4070bC9B655F6A', // WSYS wrapped native
-  oneInchRouter: null, // 1inch not deployed
-  uniswapV3Router: null // no Uni v3 router
+  dustclaim: '0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc',
+  weth: '0x7C598c96D02398d89FbCb9d41Eab3DF0C16F227D', // WSYS wrapped native
+  zeroXHost: null, // 0x not yet supported on Syscoin
+  support0x: false,
 },
 
 
@@ -231,8 +227,8 @@ export const DEPLOYMENTS = {
   name: 'XDC Network',
   dustclaim: '0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46',
   weth: '0xE7C9C6dc2A1FDB7B70950f0bA27be11Cbb9dBa1D', // WXDC
-  oneInchRouter: null, // not supported on XDC
-  uniswapV3Router: null // no Uni v3 deployment
+  zeroXHost: null, // 0x not yet supported on XDC
+  support0x: false,
 },
 
 61: {
@@ -240,117 +236,117 @@ export const DEPLOYMENTS = {
   name: "Ethereum Classic",
   dustclaim: "0x7692fDf5bbcA49ACE485D31B760e8A082d193D3d",
   weth: "0x82A618305706B14e7bcf2592D4B9324A366b6dAd", // WETC
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // no 1inch
-  uniswapV3Router: null // no Uni v2/v3
+  zeroXHost: null, // 0x not yet supported on ETC
+  support0x: false,
 },
 
 57073: {
   chainId: 57073,
   name: "Inkonchain",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006", // WETH on Ink
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // no 1inch on Ink
-  uniswapV3Router: "0x177778F19E89dD1012BdBe603F144088A95C4B53" // SwapRouter02 (exactInputSingle)
+  zeroXHost: 'https://inkonchain.api.0x.org/', // 0x supported on Ink
+  support0x: true,
 },
 
 122: {
   chainId: 122,
   name: "Fuse Network",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
-  weth: "0x5622F6dC93e08a8b717B149677930C38d5d50682", // WFUSE
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // no 1inch
-  uniswapV3Router: "0x0000000000000000000000000000000000000000" // no Uni v2/v3
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
+  weth: "0xa722c13135930332Eb3d749B2F0906559D2C5b99", // WETH Bridge Token
+  zeroXHost: null, // 0x not yet supported on Fuse
+  support0x: false,
 },
 
 1868: {
   chainId: 1868,
   name: "Soneium",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006", // WETH (verified by docs) 1
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // no confirmed 1inch router
-  uniswapV3Router: null // no confirmed Uni v3 router
+  zeroXHost: null, // 0x not yet supported on Soneium
+  support0x: false,
 },
 
 60808: {
   chainId: 60808,
   name: "BOB Mainnet",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006", // wrapped native 
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // assume none for now
-  uniswapV3Router: null // assume none for now
+  zeroXHost: null, // 0x not yet supported on BOB
+  support0x: false,
 },
 
 81457: {
   chainId: 81457,
   name: "Blast Mainnet",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth:"0x4300000000000000000000000000000000000004",
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // not yet confirmed
-  uniswapV3Router: null // not yet confirmed
+  zeroXHost: 'https://blast.api.0x.org/', // 0x supported on Blast  
+  support0x: true,
 },
 
 480: {
   chainId: 480,
   name: "World Chain",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006",
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // 1inch not yet confirmed
-  uniswapV3Router: " 0x091AD9e2e6e5eD44c1c66dB50e49A601F9f36cF6"
+  zerooXHost: 'https://worldchain.api.0x.org/', // 0x supported on Worldchain
+  support0x: true,
 },
 
 1135: {
   chainId: 1135,
   name: "Lisk",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006",
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // 1inch not confirmed
-  uniswapV3Router: null // Uni v3 router not confirmed
+  zeroXHost: null,
+  support0x: false,
 },
 
 1923: {
   chainId: 1923,
   name: "Swellchain",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006",
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // 1inch not confirmed
-  uniswapV3Router: null // Uniswap v2/v3 router not confirmed
+  zeroXHost: null,
+  support0x: false,
 },
 
 2741: {
   chainId: 2741,
   name: "Abstract",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46", 
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc", 
   weth: "0x4200000000000000000000000000000000000006", // ABETH (wrapped native)
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // 1inch NOT on Abstract
-  uniswapV3Router: null // No Uni v3 router yet
+  zeroXHost: null,
+  support0x: false,
 },
 
 747474: {
   chainId: 747474,
   name: "Katana",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006",
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // 1inch not confirmed
-  uniswapV3Router: null // Uniswap v3 router not confirmed
+  zeroXHost: null,
+  support0x: false,
 },
 
 146: {
   chainId: 146,
   name: "Sonic",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   // Native wrapper on Sonic (wS), not bridged WETH
   weth: "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38", // Wrapped S (wS)
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // no 1inch on Sonic
-  uniswapV3Router: null // no confirmed Uni v2/v3 router address yet
+  zeroXHoast: 'https://sonic.api.0x.org/', // 0x supported on Sonic
+  support0x: true,
 },
 
 534352: {
   chainId: 534352,
   name: "Scroll",
-  dustclaim: "0xd63C9015624491f6Ba7cC137E46D8dF2132F2b46",
+  dustclaim: "0xe7A0350d7D99441A0f67B4E4e7f8391f09c574dc",
   weth: "0x4200000000000000000000000000000000000006", // WETH on Scroll
-  oneInchRouter: "0x0000000000000000000000000000000000000000", // no 1inch on Scroll
-  uniswapV3Router: null // no Uni v3 router on Scroll
+  zeroXHost: 'https://scroll.api.0x.org/', // 0x supported on Scroll
+  support0x: true,
 }
 
   }
--- src/services/claimExecutor.js
+++ src/services/claimExecutor.js
@@ -1,127 +1,93 @@
 // src/services/claimExecutor.js
 import axios from 'axios'
 import walletService from './walletService'
-import { erc20Abi, encodeFunctionData } from 'viem'
+import { encodeFunctionData } from 'viem'
+import DustClaimABI from '../config/contracts/dustclaim.common.json'
+import { DEPLOYMENTS } from '../config/deployments'
 
-// 0x Swap API hosts per chain (19 supported chains)
+// -------------------------------
+// 0x Swap API hosts (quote only)
+// -------------------------------
 export const ZEROX_HOST_BY_CHAIN = {
-  1: 'https://api.0x.org',                 // Ethereum (Mainnet)
-
-  10: 'https://optimism.api.0x.org',        // Optimism
-  56: 'https://bsc.api.0x.org',             // BSC
-  130: 'https://unichain.api.0x.org',       // Unichain
-  137: 'https://polygon.api.0x.org',        // Polygon
-  143: 'https://monad.api.0x.org',          // Monad
-  146: 'https://sonic.api.0x.org',          // Sonic
-  480: 'https://worldchain.api.0x.org',     // World Chain
-  5000: 'https://mantle.api.0x.org',        // Mantle
-  9745: 'https://plasma.api.0x.org',        // Plasma
-
-  42161: 'https://arbitrum.api.0x.org',     // Arbitrum
-  43114: 'https://avalanche.api.0x.org',    // Avalanche
-  534352: 'https://scroll.api.0x.org',      // Scroll
-  59144: 'https://linea.api.0x.org',        // Linea
-
-  80094: 'https://berachain.api.0x.org',    // Berachain
-  81457: 'https://blast.api.0x.org',        // Blast
-  34443: 'https://mode.api.0x.org',         // Mode
-  8453: 'https://base.api.0x.org',          // Base
-  57073: 'https://ink.api.0x.org',          // Ink
+  1: 'https://api.0x.org',
+  10: 'https://optimism.api.0x.org',
+  56: 'https://bsc.api.0x.org',
+  130: 'https://unichain.api.0x.org',
+  137: 'https://polygon.api.0x.org',
+  143: 'https://monad.api.0x.org',
+  146: 'https://sonic.api.0x.org',
+  480: 'https://worldchain.api.0x.org',
+  5000: 'https://mantle.api.0x.org',
+  9745: 'https://plasma.api.0x.org',
+  42161: 'https://arbitrum.api.0x.org',
+  43114: 'https://avalanche.api.0x.org',
+  534352: 'https://scroll.api.0x.org',
+  59144: 'https://linea.api.0x.org',
+  80094: 'https://berachain.api.0x.org',
+  81457: 'https://blast.api.0x.org',
+  34443: 'https://mode.api.0x.org',
+  8453: 'https://base.api.0x.org',
+  57073: 'https://ink.api.0x.org'
 }
 
-// -------------------------------
-// Public: execute one chain plan
-// -------------------------------
-/**
- * Executes a chain plan: optional approvals then swap txs.
- * @param {Object} chainPlan - { chainId: number, steps: Step[] }
- * @param {string=} fromAddress - optional; will be read from wallet if omitted
- * @returns {Promise<Array<{type:'approval'|'swap', ok:boolean, txHash?:string, error?:string}>>}
- */
+// --------------------------------------------------
+// Execute chain plan THROUGH DustClaimV3
+// --------------------------------------------------
 export async function executeChainPlan(chainPlan, fromAddress) {
   const receipts = []
 
-  // 0) Ensure wallet is connected & on the right chain
-  const connected = await walletService.isConnected?.()
+  const connected = await walletService.isConnected()
   if (!connected) throw new Error('Wallet not connected')
 
-  const currentChainHex = await walletService.getChainId?.()
-  const currentChainId =
-    typeof currentChainHex === 'string'
-      ? parseInt(currentChainHex, 16)
-      : Number(currentChainHex || 0)
+  const currentChainHex = await walletService.getChainId()
+  const currentChainId = parseInt(currentChainHex, 16)
 
-  if (Number(chainPlan.chainId) !== Number(currentChainId)) {
-    const sw = await walletService.switchChain(Number(chainPlan.chainId))
-    if (!sw?.success) {
-      throw new Error(sw?.error || `Failed to switch to chain ${chainPlan.chainId}`)
-    }
+  if (Number(chainPlan.chainId) !== currentChainId) {
+    const sw = await walletService.switchChain(chainPlan.chainId)
+    if (!sw?.success) throw new Error('Chain switch failed')
   }
 
   const from =
     fromAddress ||
-    (await walletService.getAddress?.()) ||
-    (await (async () => {
-      const accs = await walletService.getAccounts?.()
-      return accs?.[0] || null
-    })())
+    (await walletService.getAddress()) ||
+    (await walletService.getAccounts())?.[0]
 
-  if (!from) throw new Error('No sender address available')
+  if (!from) throw new Error('No wallet address')
 
-  // 1) Execute each step
+  const deployment = DEPLOYMENTS[chainPlan.chainId]
+  if (!deployment?.dustclaim) {
+    throw new Error(`DustClaimV3 not deployed on chain ${chainPlan.chainId}`)
+  }
+
+  // Execute each dust swap THROUGH the contract
   for (const step of chainPlan.steps) {
-    // --- 1a) Approval (if needed and not using permit) ---
-    if (step.needsApproval && !step.usePermit) {
-      // If we don't have a spender, skip approval instead of killing the flow
-      if (!step.spender) {
-        console.warn(
-          '[executeChainPlan] step.needsApproval=true but no step.spender set – skipping approval for this step',
-          step
-        )
-      } else {
-        try {
-          const data = encodeFunctionData({
-            abi: erc20Abi,
-            functionName: 'approve',
-            args: [step.spender, BigInt(step.amount)]
-          })
-
-          const res = await walletService.sendTransaction({
-            to: step.tokenIn,
-            from,
-            data
-          })
-
-          receipts.push({
-            type: 'approval',
-            ok: !!res.success,
-            txHash: res.txHash,
-            error: res.error
-          })
-
-          // If approval failed, skip the swap for this step
-          if (!res.success) continue
-        } catch (err) {
-          receipts.push({
-            type: 'approval',
-            ok: false,
-            error: err?.message || 'Approval failed'
-          })
-          // Skip swap if approval failed
-          continue
-        }
-      }
-    }
-
-    // --- 1b) Swap (0x only now) ---
     try {
       if (step.aggregator !== '0x') {
-        throw new Error(`Unsupported aggregator: ${step.aggregator || 'none'} (only 0x supported now)`)
+        throw new Error('Only 0x supported')
       }
 
-      const tx = await build0xSwapTx(Number(chainPlan.chainId), step, from)
+      // 1) Ask 0x for calldata (taker = DustClaimV3)
+      const { swapCalldata, allowanceTarget } =
+        await build0xCalldata(chainPlan.chainId, step, deployment.dustclaim)
 
-      const res = await walletService.sendTransaction(tx)
+      // 2) Encode DustClaimV3 call
+      const data = encodeFunctionData({
+        abi: DustClaimABI,
+        functionName: 'claimDustUsingAggregator',
+        args: [
+          step.tokenIn,
+          BigInt(step.amount),
+          allowanceTarget,
+          swapCalldata
+        ]
+      })
+
+      // 3) Send tx → DustClaimV3
+      const res = await walletService.sendTransaction({
+        from,
+        to: deployment.dustclaim,
+        data
+      })
 
       receipts.push({
         type: 'swap',
@@ -133,7 +99,7 @@ export async function executeChainPlan(chainPlan, fromAddress) {
       receipts.push({
         type: 'swap',
         ok: false,
-        error: err?.response?.data?.message || err?.message || 'Swap failed'
+        error: err?.response?.data?.message || err.message
       })
     }
   }
@@ -141,40 +107,31 @@ export async function executeChainPlan(chainPlan, fromAddress) {
   return receipts
 }
 
-// -------------------------------
-// Helpers: 0x tx builder
-// -------------------------------
+// --------------------------------------------------
+// 0x helper (quote only)
+// --------------------------------------------------
+async function build0xCalldata(chainId, step, takerAddress) {
+  const host = ZEROX_HOST_BY_CHAIN[chainId]
+  if (!host) throw new Error(`0x not supported on ${chainId}`)
 
-/**
- * Build a sendable tx for 0x /swap/v1/quote.
- * Uses step.{tokenIn, tokenOut, amount, slippage} and takerAddress = from.
- */
-async function build0xSwapTx(chainId, step, from) {
-  const host = ZEROX_HOST_BY_CHAIN[Number(chainId)]
-  if (!host) {
-    throw new Error(`0x not supported on chain ${chainId}`)
-  }
-
-  const slippagePct = step.slippage != null ? Number(step.slippage) : 1 // 1% default
+  const slippagePct = step.slippage != null ? step.slippage : 1
 
   const { data } = await axios.get(`${host}/swap/v1/quote`, {
     params: {
       sellToken: step.tokenIn,
       buyToken: step.tokenOut,
-      sellAmount: String(step.amount), // wei (string)
-      takerAddress: from,
+      sellAmount: String(step.amount),
+      takerAddress,
       slippagePercentage: slippagePct / 100
     }
   })
 
-  if (!data?.to || !data?.data) {
-    throw new Error('Malformed 0x quote response')
+  if (!data?.data || !data?.allowanceTarget) {
+    throw new Error('Invalid 0x response')
   }
 
   return {
-    from,
-    to: data.to,
-    data: data.data,
-    value: data.value ?? '0x0'
+    swapCalldata: data.data,
+    allowanceTarget: data.allowanceTarget
   }
 }
