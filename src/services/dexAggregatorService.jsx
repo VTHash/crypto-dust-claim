ffddae7755919f07a134f8183d382e99064516e8
VTHash <hfv@highfinanceventures.com>
2026-01-02 11:16:08 +0000

    Add new HOST_BY_CHAIN list and all changes

--- src/services/dexAggregatorService.jsx
+++ src/services/dexAggregatorService.jsx
@@ -55,12 +55,29 @@ const WRAPPED_NATIVE_BY_CHAIN = {
  * 0x API hosts are chain-specific. Use the correct base for each chain.
  */
 const ZEROX_HOST_BY_CHAIN = {
-  1: 'https://api.0x.org',
-  10: 'https://optimism.api.0x.org',
-  56: 'https://bsc.api.0x.org',
-  137: 'https://polygon.api.0x.org',
-  42161: 'https://arbitrum.api.0x.org',
-  8453: 'https://base.api.0x.org',
+   1: 'https://api.0x.org',                 // Ethereum (Mainnet)
+
+  10: 'https://optimism.api.0x.org',        // Optimism
+  56: 'https://bsc.api.0x.org',             // BSC
+  130: 'https://unichain.api.0x.org',       // Unichain
+  137: 'https://polygon.api.0x.org',        // Polygon
+  143: 'https://monad.api.0x.org',          // Monad
+  146: 'https://sonic.api.0x.org',          // Sonic
+  480: 'https://worldchain.api.0x.org',     // World Chain
+  5000: 'https://mantle.api.0x.org',        // Mantle
+  9745: 'https://plasma.api.0x.org',        // Plasma
+
+  42161: 'https://arbitrum.api.0x.org',     // Arbitrum
+  43114: 'https://avalanche.api.0x.org',    // Avalanche
+  534352: 'https://scroll.api.0x.org',      // Scroll
+  59144: 'https://linea.api.0x.org',        // Linea
+
+  80094: 'https://berachain.api.0x.org',    // Berachain
+  81457: 'https://blast.api.0x.org',        // Blast
+  34443: 'https://mode.api.0x.org',         // Mode
+  8453: 'https://base.api.0x.org',          // Base
+  57073: 'https://ink.api.0x.org',          // Ink
+
 }
 
 /** Normalize amount to a decimal string (wei) for HTTP calls */
